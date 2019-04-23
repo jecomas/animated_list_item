de8fe885a1da7f06e1a391077470dc6da9e844ee
jecomas <javier@leapfactor.com>
2019-04-23 15:22:33 -0300

    refactor and simplify

---
+++ lib/data.dart
@@ -0,0 +1,4 @@
+const double ITEM_HEIGHT = 70.0;
+List<String> dataOrigin = ['Leapfactor'];
+List<String> dataAmount = ['125.50'];
+List<String> dataDate = ['14/04/2019'];
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'list_item.dart';
+import 'data.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,87 +8,76 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Animated List',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: BodyLayout(),
+      home: MyHomePage(title: 'Animated List'),
     );
   }
 }
 
-class BodyLayout extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
   @override
-  BodyLayoutState createState() {
-    return new BodyLayoutState();
-  }
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class BodyLayoutState extends State<BodyLayout> {
-  
-  final GlobalKey<AnimatedListState> _listKey = GlobalKey();
+class _MyHomePageState extends State<MyHomePage> {
+  ScrollController _scrollController = new ScrollController();
 
-  List<String> _dataOrigin = ['Coca Cola', 'Leapfactor', 'Amazon'];
-  List<String> _dataAmount = ['500', '125.50', '1235'];
-  List<String> _dataDate = ['10/04/2019', '14/04/2019', '15/04/2019'];
+  void _addItem() {
+    setState(() {
+      dataOrigin.insert(0, 'Otra');
+      dataAmount.insert(0, '1255');
+      dataDate.insert(0, '15/04/22');
+      _scrollController.animateTo(dataOrigin.length * ITEM_HEIGHT, duration: new Duration(seconds: 2), curve: Curves.ease);
+    });
+  }
+
+  void _removeItem() {
+    setState(() {
+      dataOrigin.removeAt(0);
+      dataAmount.removeAt(0);
+      dataDate.removeAt(0);
+      _scrollController.animateTo(dataOrigin.length * ITEM_HEIGHT, duration: new Duration(seconds: 2), curve: Curves.ease);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        SizedBox(
-          height: 500,
-          width: 700,
-          child: AnimatedList(
-            key: _listKey,
-            initialItemCount: _dataOrigin.length,
-            itemBuilder: (context, index, animation) {
-              return ListItem(paymentOrigin: _dataOrigin[index], paymentAmount: _dataAmount[index], paymentDate: _dataDate[index]);
-            },
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Align(
+          alignment: Alignment.topCenter,
+          child: new ListView.builder(
+              controller: _scrollController,
+              reverse: true,
+              shrinkWrap: true,
+              scrollDirection: Axis.vertical,
+              itemCount: dataOrigin.length,
+              itemBuilder: (context, index) {
+                return ListItem(paymentOrigin: dataOrigin[index], paymentAmount: dataAmount[index], paymentDate: dataDate[index]);
+              }),
+        ),
+        floatingActionButton: Column(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
+          FloatingActionButton(
+            onPressed: _addItem,
+            tooltip: 'Add',
+            child: Icon(Icons.add),
           ),
-        ),
-        RaisedButton(
-          child: Text('Insert item', style: TextStyle(fontSize: 20)),
-          onPressed: () {
-            _insertSingleItem();
-          },
-        ),
-        RaisedButton(
-          child: Text('Remove item', style: TextStyle(fontSize: 20)),
-          onPressed: () {
-            _removeSingleItem();
-          },
-        )
-      ],
-    );
-  }
-
-  void _insertSingleItem() {
-    String newItem = "Leapfactor";
-    String newItemA = "125";
-    String newItemD = "14/03/2019";
-    // Arbitrary location for demonstration purposes
-    int insertIndex = _dataOrigin.length;
-    // Add the item to the data list.
-    _dataOrigin.insert(insertIndex, newItem);
-    _dataAmount.insert(insertIndex, newItemA);
-    _dataDate.insert(insertIndex, newItemD);
-    // Add the item visually to the AnimatedList.
-    _listKey.currentState.insertItem(insertIndex);
-  }
-
-  void _removeSingleItem() {
-    int removeIndex = _dataOrigin.length - 1;
-    // Remove item from data list but keep copy to give to the animation.
-    String removedItem = _dataOrigin.removeAt(removeIndex);
-    String removedItemA = _dataAmount.removeAt(removeIndex);
-    String removedItemD = _dataDate.removeAt(removeIndex);
-    // This builder is just for showing the row while it is still
-    // animating away. The item is already gone from the data list.
-    AnimatedListRemovedItemBuilder builder = (context, animation) {
-      return ListItem(paymentOrigin: removedItem, paymentAmount: removedItemA, paymentDate: removedItemD);
-    };
-    // Remove the item visually from the AnimatedList.
-    _listKey.currentState.removeItem(removeIndex, builder);
+          SizedBox(height: 10),
+          FloatingActionButton(
+            onPressed: _removeItem,
+            tooltip: 'Remove',
+            child: Icon(Icons.remove),
+          ),
+        ])
+        // This trailing comma makes auto-formatting nicer for build methods.
+        );
   }
 }
